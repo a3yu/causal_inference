da702d344c1e0f9fb001f9b29952f9fbe749a17b
a3yu <74028906+a3yu@users.noreply.github.com>
2024-03-14 17:47:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def main():
     # Z = staggered_bern(n, P, r)
     # TTE_est = poly_interp(Z, Y, P)
     P = np.array([0.5, 0.7])
-    Z = rct.staggered_Bernoulli(5, P, 3)
-    print(Z)
+    Z = rct.staggered_Bernoulli(3, P, 5)
     
+    a = pom.inner(Z, [[0,1,2],[0,1],[2]],[[0,1,2,3,4,0],[0,1,2,0,0,0],[0,1,0,0,0,0]],1 )
+    print(Z)
+    print(Z.shape)
 
 if __name__ == '__main__':
     main()
