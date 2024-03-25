488fe9b18d804df4154c8f451894dbe183789a84
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-03-25 12:18:29 -0400

    added method to create partitions and edge probability matrix for the SBM

--- model/graphs/graphs.py
+++ model/graphs/graphs.py
@@ -1,4 +1,36 @@
 import random
+import numpy as np
+
+def balanced_partition_pin_pout(n : int, nc : int, p_in : float, p_out: float):
+    '''
+    Returns partitions (list of "equal-sized" partitions of [n]) and probs (nc by nc numpy array with p_in on diagonal and p_out on offdiagonal)
+    
+    n : number of units
+    nc: number of partitions/clusters/groups
+    p_in: edge probability within clusters
+    p_out: edge probability across clusters
+
+    e.g. if n = 10 & nc = 2, partitions = [[0,1,2,3,4], [5,6,7,8,9]]
+    if n = 10 & nc = 3, partitions = [[0,1,2,3], [4,5,6], [7,8,9]]
+    if n = 10 & nc = 4, partitions = [[0,1,2], [3,4,5], [6,7], [8,9]]
+    if n = 10 & nc = 5, partitions = [[0,1,2,3,4], [5,6,7,8,9]]
+    '''
+    probs = np.full((nc, nc), p_out, dtype=float)
+    np.fill_diagonal(probs, p_in)
+
+    if n % nc == 0: # we can split units evenly into nc clusters
+        partition_size = n // nc
+        partitions = [list(range(i * partition_size, (i + 1) * partition_size)) for i in range(nc)]
+    else: # we cannot split into equal-sized; (n % nc) clusters will have (n//nc)+1 units; the remaining clusters will have (n//nc) units
+        larger_partitions = n % nc
+        smaller_partitions = nc - larger_partitions
+        larger_partition_size = n // nc + 1
+        smaller_partition_size = n // nc
+
+        partitions = [list(range(i * larger_partition_size, (i + 1) * larger_partition_size)) for i in range(larger_partitions)]
+        partitions += [list(range(larger_partitions * larger_partition_size + (i * smaller_partition_size),
+                                    larger_partitions * larger_partition_size + ((i+1) * smaller_partition_size))) for i in range(smaller_partitions)]
+    return partitions, probs
 
 def SBM(size, partition, probabilities):
     '''
