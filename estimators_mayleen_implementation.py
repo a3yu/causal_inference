e26345ffd49b875912f55bce84ab417878708abb
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-04-11 14:48:35 -0400

    deleted some functions we already newly implemented

--- estimators_mayleen_implementation.py
+++ estimators_mayleen_implementation.py
@@ -1,42 +1,6 @@
 import numpy as np
 import scipy.sparse
 
-def PI(n, sums, H):
-    '''
-    Returns an estimate of the TTE with (beta+1) staggered rollout design
-
-    n (int): popluation size
-    H (numpy array): PPOM coefficients h_t or l_t
-    sums (numpy array): sums of outcomes at each time step
-    '''
-    if n > 0:
-        return (1/n)*H.dot(sums)
-    else:
-        return 0
-    
-def outcome_sums(n, Y, Z, selected):
-    '''
-    Returns the sums of the outcomes Y(z_t) for each timestep t
-
-    Y (function): potential outcomes model
-    Z (numpy array): treatment vectors z_t for each timestep t
-    - each row should correspond to a timestep, i.e. Z should be beta+1 by n
-    selected (list): indices of units in the population selected to be part of the experiment (i.e in U)
-    '''
-    if len(selected) == n: # if we selected all nodes, sums = sums_U
-        sums = np.zeros(Z.shape[0])
-        for t in range(Z.shape[0]):
-            outcomes = Y(Z[t,:])
-            sums[t] = np.sum(outcomes)
-        return sums, sums
-    else: 
-        sums, sums_U = np.zeros(Z.shape[0]), np.zeros(Z.shape[0]) 
-        for t in range(Z.shape[0]):
-            outcomes = Y(Z[t,:])
-            sums[t] = np.sum(outcomes)
-            sums_U[t] = np.sum(outcomes[selected])
-    return sums, sums_U
-
 def poly_LS_prop(beta, y, A, z):
   '''
   Returns an estimate of the TTE using polynomial regression using
@@ -146,46 +110,6 @@ def DM_fraction(n, y, A, z, tol):
 #######################################
 # Estimators - Horvitz-Thomson & Hajek
 #######################################
-
-def horvitz_thompson_old(n, nc, y, A, z, q, p):
-    '''Computes the Horvitz-Thompson estimate of the TTE under Bernoulli design or Cluster-Bernoulli design.
-    
-    Parameters
-    ----------
-    n : int
-        the size of the population/network
-    nc : int
-        the number of clusters (equals n if simple Bernoulli design with no clustering)
-    y : numpy array
-        the outcomes of each unit in the population
-    A : scipy sparse array
-        adjacency matrix of the network such that A[i,j]=1 indicates that unit j is an in-neighbor of i
-    z : numpy array
-        the treatment assignment of each unit in the population
-    q : float
-        probability that a cluster is indepdently chosen for treatment (should equal 1 under simple Bernoulli design with no clustering)
-    p : float
-        the treatment probability for chosen clusters in the staggered rollout
-    '''
-    neighborhoods = [list(row.nonzero()[1]) for row in A] # list of neighbors of each unit
-    neighborhood_sizes = A.sum(axis=1).tolist() # size of each unit's neighborhood
-    neighbor_treatments = [list(z[neighborhood]) for neighborhood in neighborhoods] # list of treatment assignments in each neighborhood
-
-    A = A.multiply(scipy.sparse.csr_array(np.tile(np.repeat(np.arange(1,nc+1),n//nc), (n,1)))) # modifies the adjancecy matrix so that if there's an edge from j to i, A[i,j]=cluster(j)
-    cluster_neighborhoods = [np.unique(row.data,return_counts=True) for row in A] # for each i, cluster_neighborhoods[i] = [a list of clusters i's neighbors belong to, a list of how many neighbors are in each of these clusters]
-    cluster_neighborhood_sizes = [len(x[0]) for x in cluster_neighborhoods] # size of each unit's cluster neighborhood
-    
-    # Probabilities of each person's neighborhood being entirely treated or entirely untreated
-    all_treated_prob = np.multiply(np.power(p, neighborhood_sizes), np.power(q, cluster_neighborhood_sizes))
-    none_treated_prob = [np.prod((1-q) + np.power(1-p, x[1])*q) for x in cluster_neighborhoods]
-    
-    # Indicators of each person's neighborhood being entirely treated or entirely untreated
-    all_treated = [np.prod(treatments) for treatments in neighbor_treatments]
-    none_treated = [all(z == 0 for z in treatments)+0 for treatments in neighbor_treatments]
-
-    zz = np.nan_to_num(np.divide(all_treated,all_treated_prob) - np.divide(none_treated,none_treated_prob))
-
-    return 1/n * y.dot(zz)
 
 def horvitz_thompson(n, nc, y, A, z, q, p):    
     AA = A.toarray()
