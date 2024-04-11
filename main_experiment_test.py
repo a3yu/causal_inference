b95cb66604ec599210a25d7fa8ff77960ce6bc5a
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-04-11 13:30:16 -0400

    moved params around; commented print statements; average over graphs

--- main_experiment_test.py
+++ main_experiment_test.py
@@ -1,41 +1,42 @@
 import numpy as np
-from model.graphs.graphs import SBM, balanced_partition_pin_pout
+from model.graphs import SBM, balanced_partition_pin_pout
 from experiment import rct
 from model import pom
 from experiment.estimators import estimator
 
 def main():
-    n = 1000          # population size
     beta = 1        # potential outcomes model degree
     p = 0.5         # treatment budget (marginal treatment probability     
-
+    
+    n = 1000        # population size
     nc = 50         # number of clusters
-    avg_deg = 10     # average graph degree
+    avg_deg = 10    # average graph degree
     p_in = 0.5      # probability of edge between units within same cluster; largest this can be is (nc/n)*avg_degree
     p_out = (avg_deg - p_in*(n/nc))/(n - (n/nc))        # probability of edge between units of different clusters
+    
     cf = lambda i, S, G: pom.uniform_coeffs(i, S, G)    # function to generate coefficients
 
-    gr = 1         # number of graph repetitions
-    r = 200         # number of RCT repetitions
+    gr = 10         # number of graph repetitions
+    r = 50          # number of RCT repetitions
 
     partitions, probs = balanced_partition_pin_pout(n, nc, p_in, p_out)
+    avg_rel_bias = np.zeros(gr)
     for g in range(gr):
         G = SBM(n, partitions, probs)
         TTE_true, fy = pom.ppom(beta, G, cf)
-        print(TTE_true)
+        #print(TTE_true)
         #selected = rct.select_clusters_bernoulli(nc, p/q)
 
         P = rct.sequential_treatment_probs(beta, p)
         Z = rct.staggered_Bernoulli(n, P, r)
-        #print(Z)
         Y = fy(Z)
-        #print(Y)
-        print(np.sum(fy(np.ones((1,n,1))) - fy(np.zeros((1,n,1))))/n)
+        #print(np.sum(fy(np.ones((1,n,1))) - fy(np.zeros((1,n,1))))/n)
         
         TTE_est = estimator.polynomial_estimate(Z, Y, P) 
-        #print(TTE_est) 
-        avg_rel_bias = np.sum((TTE_est - TTE_true)/TTE_true)/r
-        print("Under graph {}, the average relative bias of the estimator is {}".format(g, avg_rel_bias))
+        avg_rel_bias[g] = np.sum((TTE_est - TTE_true)/TTE_true)/r
+    
+    avg_rel_bias_graphs = np.sum(avg_rel_bias)/gr
+    print("The average relative bias of the estimator, across {} graphs and {} RCT repetitions per graph is {}".format(gr, r, avg_rel_bias_graphs))
 
 if __name__ == '__main__':
     main()
