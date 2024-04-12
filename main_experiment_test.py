8468a23b3adaece35c020880f41ff3875747aa97
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-04-12 09:23:19 -0400

    changed to use simple SBM

--- main_experiment_test.py
+++ main_experiment_test.py
@@ -1,30 +1,30 @@
 import numpy as np
-from model.graphs import SBM, balanced_partition_pin_pout
+#from model.graphs import SBM, balanced_partition_pin_pout
+from model.graph import *
 from experiment import rct
 from model import pom
 from experiment.estimators import estimator
 
 ### Network parameters
-n = 200         # population size
-nc = 40         # number of clusters
+n = 1000         # population size
+nc = 50         # number of clusters
 p_in = 0.5      # probability of edge between units within same cluster; largest this can be is (nc/n)*avg_degree
 p_out = 0       # probability of edge between units of different clusters
-partitions, probs = balanced_partition_pin_pout(n, nc, p_in, p_out)
 
 ### Other parameters
 beta = 2        # potential outcomes model degree
 p = 0.5         # treatment budget (marginal treatment probability)     
-gr = 1          # number of graph repetitions
-r = 10000       # number of RCT repetitions
+gr = 10          # number of graph repetitions
+r = 100       # number of RCT repetitions
 cf = lambda i, S, G: pom.uniform_coeffs(i, S, G)    # function to generate coefficients
 
 ### Experiment
 bias = np.zeros(gr)
 variance = np.zeros(gr)
 for g in range(gr):
-    G = SBM(n, partitions, probs)
+    G = SimpleSBM(n, nc, p_in, p_out)
     TTE_true, outcomes = pom.ppom(beta, G, cf)
-    print(TTE_true)
+    #print(TTE_true)
 
     P = rct.sequential_treatment_probs(beta, p)
     Z = rct.staggered_Bernoulli(n, P, r)
