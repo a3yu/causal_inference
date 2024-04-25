3744ee35995f6179a6ceefef866b3f6174ef63c2
Benson Zhang <bjz22@cornell.edu>
2024-04-25 15:40:20 -0400

    dm run

--- runners/main_experiment_test.py
+++ runners/main_experiment_test.py
@@ -10,7 +10,7 @@ from experiment.estimators import estimator
 import time
 
 ### Network parameters
-n = 1000         # population size
+n = 100         # population size
 nc = 50         # number of clusters
 p_in = 0.5      # probability of edge between units within same cluster; largest this can be is (nc/n)*avg_degree
 p_out = 0       # probability of edge between units of different clusters
@@ -25,6 +25,7 @@ cf = lambda i, S, G: pom.uniform_coeffs(i, S, G)    # function to generate coeff
 ### Experiment
 start = time.time()
 bias = np.zeros(gr)
+dm_bias = np.zeros(gr)
 variance = np.zeros(gr)
 for g in range(gr):
     G = SimpleSBM(n, nc, p_in, p_out)
@@ -37,12 +38,13 @@ for g in range(gr):
 
     #print(np.sum(outcomes(np.ones((1,n,1))) - outcomes(np.zeros((1,n,1))))/n)
     TTE_dm = estimator.dm_estimate(Z, Y)
-    bias[g] = np.sum(TTE_dm - TTE_true) / r
-    print(bias[g])
+    dm_bias[g] = np.sum(TTE_dm - TTE_true) / r
     TTE_est = estimator.polynomial_estimate(Z, Y, P)
     bias[g] = np.sum(TTE_est - TTE_true)/r
     variance[g] = np.sum((TTE_est - TTE_true)**2)/r
 
+avg_bias_dm = np.sum(dm_bias) / gr
+print('DM avg bias:', avg_bias_dm)
 avg_bias = np.sum(bias)/gr
 avg_variance = np.sum(variance)/gr
 end = time.time()
