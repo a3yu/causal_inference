f86b8b0f6bb14b34c1e3b33a2be6593f5ffb7539
a3yu <74028906+a3yu@users.noreply.github.com>
2024-05-02 15:09:10 -0400

    added masterplot and new modular experiments

--- runners/modular_experiment.py
+++ runners/modular_experiment.py
@@ -1,9 +1,6 @@
 import sys
 import os
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model.graph import SimpleSBM
@@ -11,7 +8,7 @@ from experiment import rct
 from model import pom
 from experiment import estimator
 
-def run_experiment(parameters, update_param, stop_condition, track_param):
+def run_experiment_poly(parameters, update_param, stop_condition, track_param):
     '''
     parameters: dict containing initial experiment parameters
     update_param: lambda function that takes parameters and a control variable to update the parameters
@@ -21,11 +18,10 @@ def run_experiment(parameters, update_param, stop_condition, track_param):
     bias = []
     variance = []
     current_params = parameters.copy()
-    tracked_values = []  # For plotting, collect tracked parameter values
+    tracked_values = [] 
 
     while not stop_condition(current_params):
         n = current_params['n']
-        print(n)
         tracked_values.append(current_params[track_param])  # Collect values for plotting based on track_param
         nc = current_params['nc']
         p_in = current_params['p_in']
@@ -57,41 +53,54 @@ def run_experiment(parameters, update_param, stop_condition, track_param):
 
         bias.append(avg_bias)
         variance.append(avg_variance)
-
         # Update parameters for next iteration
         current_params = update_param(current_params)
 
     return tracked_values, bias, variance
 
-# Parameters dictionary
-params = {
-    'n': 500,
-    'nc': 50,
-    'p_in': 0.5,
-    'p_out': 0,
-    'beta': 2,
-    'p': 0.5,
-    'gr': 10,
-    'r': 100,
-    'cf': lambda i, S, G: pom.uniform_coeffs(i, S, G)
-}
+def run_experiment_dm(parameters, update_param, stop_condition, track_param):
+    '''
+    parameters: dict containing initial experiment parameters
+    update_param: lambda function that takes parameters and a control variable to update the parameters
+    stop_condition: lambda function that takes the current parameters and determines the end condition
+    track_param: string specifying which parameter to track for plotting
+    '''
+    bias = []
+    variance = []
+    current_params = parameters.copy()
+    tracked_values = []  # For plotting, collect tracked parameter values
 
-# Update function
-update_param = lambda params: {**params, 'n': params['n'] + 50, 'p': params['n']/1000}
+    while not stop_condition(current_params):
+        n = current_params['n']
+        tracked_values.append(current_params[track_param])  # Collect values for plotting based on track_param
+        nc = current_params['nc']
+        p_in = current_params['p_in']
+        p_out = current_params['p_out']
+        beta = current_params['beta']
+        p = current_params['p']
+        gr = current_params['gr']
+        r = current_params['r']
+        cf = current_params['cf']
 
-# Stop condition based on parameters
-stop_condition = lambda params: params['n'] > 1000  # Stop when number of units exceeds 1000
+        dm_bias = np.zeros(gr)
+        dm_variance = np.zeros(gr)
+        for g in range(gr):
+            G = SimpleSBM(n, nc, p_in, p_out)
+            TTE_true, outcomes = pom.ppom(beta, G, cf)
+            P = rct.sequential_treatment_probs(beta, p)
+            Z = rct.staggered_Bernoulli(n, P, r)
+            Y = outcomes(Z)
+            TTE_dm = estimator.dm_estimate(Z, Y)
+            dm_bias[g] = np.sum(TTE_dm - TTE_true) / r
+            dm_variance[g] = np.sum((TTE_dm - TTE_true)**2)/r
 
-# Run the experiment tracking 'n'
-tracked_param = 'n'  # This can be changed to any parameter you want to track
-tracked_values, bias, variance = run_experiment(params, update_param, stop_condition, tracked_param)
+        avg_bias = np.mean(dm_bias)
+        avg_variance = np.mean(dm_variance)
+        bias.append(avg_bias)
+        variance.append(avg_variance)
+        
+        current_params = update_param(current_params)
 
-# Plotting
-plt.figure(figsize=(10, 5))
-sns.lineplot(x=tracked_values, y=bias, label='Bias')
-plt.fill_between(tracked_values, np.array(bias) - np.array(variance), np.array(bias) + np.array(variance), color='blue', alpha=0.3)
-plt.title(f'Bias and Variance Evolution with Varying {tracked_param}')
-plt.xlabel(tracked_param)
-plt.ylabel('Bias')
-plt.legend()
-plt.show()
+    return tracked_values, bias, variance
+
+
